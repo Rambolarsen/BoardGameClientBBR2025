8ae680a6cc93a64dec3bced4f1f2f0ca975a663e
Steinar Myklebost <steinar.myklebost@bouvet.no>
2025-01-17 23:08:57 +0100

    Implement super simple PlantingOptionalPhase

---
+++ GamePhases/PlantingOptional/PlantingOptionalPhase.cs
@@ -0,0 +1,16 @@
+﻿using BoardGameClientBBR2025.API;
+using BoardGameClientBBR2025.GameBoard;
+
+namespace BoardGameClientBBR2025.GamePhases.Trading;
+
+public class PlantingOptionalPhase : GamePhaseBase, IGamePhase
+{
+    private readonly PlayingClient _playingClient = new PlayingClient(new HttpClient());
+
+    public override GamePhaseEnum GamePhase => GamePhaseEnum.PlantingOptional;
+
+    public override async Task DoPhase(GameState gameState)
+    {
+        await _playingClient.EndPlanting(gameState.Name, gameState.CurrentPlayer);
+    }
+}
