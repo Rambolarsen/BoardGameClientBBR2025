30c099f46503f81c476c46ad356f407d4a2077ff
andretorv <andre.torvestad@bouvet.no>
2025-01-17 19:48:18 +0100

    getscore

---
+++ API/Score.cs
@@ -0,0 +1,32 @@
+using BoardGameClientBBR2025.GameBoard;
+using System.Text.Json;
+namespace BoardGameClientBBR2025.API
+{
+    public class Score
+    {
+        private readonly HttpClient httpClient;
+        private readonly string baseUrl = "https://localhost:5186/api/score";
+
+
+        public GetScore(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<int> GetScore()
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"{baseUrl}/get"),
+            };
+
+            using (var response = await httpClient.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<int>(body);
+            }
+        }
+    }
+}
