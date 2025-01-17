814c5d041b69ea005346f83eb3ea548e0a9fb35a
Steinar Myklebost <steinar.myklebost@bouvet.no>
2025-01-17 23:23:33 +0100

    Implement very simple TradePlantingPhase

---
+++ GamePhases/TradePlanting/TradePlantingPhase.cs
@@ -0,0 +1,32 @@
+﻿using BoardGameClientBBR2025.API;
+using BoardGameClientBBR2025.GameBoard;
+
+namespace BoardGameClientBBR2025.GamePhases.TradePlanting;
+
+public class TradePlantingPhase : GamePhaseBase, IGamePhase
+{
+    private readonly PlayingClient _playingClient = new PlayingClient(new HttpClient());
+
+    public override GamePhaseEnum GamePhase => GamePhaseEnum.TradePlanting;
+
+    public override async Task DoPhase(GameState gameState)
+    {
+        var player = gameState.Players.First(p => p.Name == gameState.CurrentPlayer);
+
+        var cardsToPlant = new List<object>();
+        cardsToPlant.AddRange(player.DrawnCards);
+        cardsToPlant.AddRange(player.TradedCards);
+
+        var firstFieldId = player.Fields.First().Key.ToString();
+
+        foreach (object card in cardsToPlant)
+        {
+            _playingClient.TradePlant(gameState.Name, gameState.CurrentPlayer, GetCardId(card), firstFieldId);
+        }
+    }
+
+    private string GetCardId(object card)
+    {
+        throw new NotImplementedException();
+    }
+}
