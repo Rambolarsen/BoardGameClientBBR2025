aa4546761f63d559ada8483bc7fe268ab011169e
Heljar <heljar.flaten@gmail.com>
2025-01-18 09:41:23 +0100

    Trading

--- GamePhases/Trading/TradingPhase.cs
+++ GamePhases/Trading/TradingPhase.cs
@@ -14,31 +14,12 @@ public class TradingPhase : GamePhaseBase, IGamePhase
 		await playingClient.EndTrading(gameName, ourPlayerId);
 	}
 
-	public async Task ConsiderTrades(string gameName, string ourPlayerId, string ourPlayerName, GameState gameState, PlayingClient playingClient)
-	{
-		foreach (var oneTrade in gameState.AvailableTrades)
-		{
-			foreach (var oneTypeOffered in oneTrade.CardTypesWanted)
-			{
-				if (_wantCards.Any(x => x.Type == oneTypeOffered))
-				{
-					foreach (var oneTypeWanted in oneTrade.CardTypesWanted)
-					{
-						if (_getRidOfCards.Any(x => x.Type == oneTypeWanted))
-						{
-						}
-					}
-				}
-			}
-		}
-	}
-
 	public void CalculatePossibleTrades(string ourPlayerId, string ourPlayerName, GameState gameState)
 	{
 		var us = gameState.GetUs(ourPlayerName);
 		var ourHand = gameState.YourHand;
 		var ourFields = us.Fields;
-		
+
 		var drawnCards = new List<Card>();
 		if (gameState.ArWeActivePlayer(ourPlayerId))
 		{
@@ -49,18 +30,74 @@ public class TradingPhase : GamePhaseBase, IGamePhase
 		_getRidOfCards = FindGetRidOfCards(ourHand, drawnCards, ourFields);
 	}
 
-	private async Task OfferTrades(string gameName, string ourPlayerId, string ourPlayerName, GameState gameState, GameClient gameClient)
+	public async Task ConsiderTrades(string gameName, string ourPlayerId, string ourPlayerName, GameState gameState, PlayingClient playingClient)
 	{
+		//foreach (var oneTrade in gameState.AvailableTrades)
+		//{
+		//	foreach (var oneTypeOffered in oneTrade.CardTypesWanted)
+		//	{
+		//		if (_wantCards.Any(x => x.Type == oneTypeOffered))
+		//		{
+		//			foreach (var oneTypeWanted in oneTrade.CardTypesWanted)
+		//			{
+		//				if (_getRidOfCards.Any(x => x.Type == oneTypeWanted))
+		//				{
+		//					playingClient.AcceptTrade(gameName, )
+		//				}
+		//			}
+		//		}
+		//	}
+		//}
+	}
 
+	private async Task OfferTrades(string gameName, string ourPlayerId, string ourPlayerName, GameState gameState, PlayingClient playingClient)
+	{
+	}
+
+	private List<Card> GetAvailableCards(List<Card> ourHand, List<Card> drawnCards)
+	{
+		var availableCards = new List<Card>();
+
+		availableCards.AddRange(ourHand);
+		availableCards.AddRange(drawnCards);
+
+		return availableCards;
+	}
+
+	private List<Card> GetCardTypesFromFields(List<Field> ourFields)
+	{
+		var fieldCardTypes = new List<Card>();
+
+		foreach (var field in ourFields)
+		{
+			if (!field.IsEmpty())
+			{
+				fieldCardTypes.Add(field.Card.First());
+			}
+		}
+
+		return fieldCardTypes;
 	}
 
 	private List<Card> FindWantCards(List<Card> ourHand, List<Card> drawnCards, List<Field> ourFields)
 	{
-		return new List<Card>();
+		return GetCardTypesFromFields(ourFields);
 	}
 
 	private List<Card> FindGetRidOfCards(List<Card> ourHand, List<Card> drawnCards, List<Field> ourFields)
 	{
-		return new List<Card>();
+		var cardsToGetRidOf = new List<Card>();
+		var cardsAvailable = GetAvailableCards(ourHand, drawnCards);
+		var cardsOnField = GetCardTypesFromFields(ourFields);
+
+		foreach (var oneAvailableCard in cardsAvailable)
+		{
+			if (cardsOnField.All(x => x.Type != oneAvailableCard.Type))
+			{
+				cardsToGetRidOf.Add(oneAvailableCard);
+			}
+		}
+
+		return cardsToGetRidOf;
 	}
 }
