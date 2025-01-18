b8c71f8dcbcf4e6c35c4ca579464c57bcc8e8c1f
Lars-Erik Arnesen <lars-erik.arnesen@bouvet.no>
2025-01-18 11:04:42 +0100

    Add user constants

--- Constants.cs
+++ Constants.cs
@@ -2,15 +2,20 @@
 {
     public static class Constants
     {
-        //public const string BaseUrl = "https://localhost:7046/api";
+        public const string BaseUrl = "https://localhost:7046/api";
 
-        public const string BaseUrl = "https://f540-193-212-67-198.ngrok-free.app/api";
+        //public const string BaseUrl = "http://f540-193-212-67-198.ngrok-free.app/api";
 
         public const string GameUrl = $"{BaseUrl}/game";
 
         public const string PlayingUrl = $"{BaseUrl}/playing";
 
         public const string ScoreUrl = $"{BaseUrl}/score";
+
+        public static readonly Guid AgentId = new Guid("44c81fd9-8124-4acf-a1bd-fd6b54a8f5ca");
+
+        public const string PlayerName = "Mjondalen";
+
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,8 +4,6 @@ using BoardGameClientBBR2025.API;
 var client = new HttpClient();
 var gameClient = new GameClient(client);
 var playingClient = new PlayingClient(client);
-const string player1 = "Mjondalen";
-const string player2 = "Mjøndalen2";
 
 var games = await gameClient.GetAllGames();
 if(games == null)
@@ -15,14 +13,11 @@ if(games == null)
 }
 
 var agents = new List<Agent>();
-var playerKey1 = Guid.NewGuid();
-
-var playerKey2 = Guid.NewGuid();
 
 var game = games.ElementAt(1);
 //foreach (var game in games)
 //{
-    agents.Add(new Agent(game, playerKey1, player1, gameClient, playingClient));
+    agents.Add(new Agent(game, Constants.AgentId, Constants.PlayerName, gameClient, playingClient));
     //agents.Add(new Agent(game, playerKey2, player2, gameClient, playingClient));
 //}
 
